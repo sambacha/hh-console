f09f18d3db4359a95a064c57ff00af3aab3523e2
sam bacha <sam@manifoldfinance.com>
2021-08-18 23:34:22 -0700

    get functionDebugData

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,34 +7,47 @@ const HARDHAT_ENABLE_SENTRY = false;
 const mnemonic = 'test test test test test test test test test test test junk';
 
 const config: HardhatUserConfig = {
-  packager: {
-    contracts: ["console"],
-    includeFactories: true,
-  },
-  paths: {
-    artifacts: "./artifacts",
-    cache: "./cache",
-    sources: "./contracts",
-    tests: "./test",
-  },
-  solidity: {
-    version: '0.8.0',
-    settings: {
-      metadata: {
-        bytecodeHash: 'none',
-      },
-      optimizer: {
-        enabled: false,
-        runs: 200,
-        details: {
-          yul: false,
+        packager: {
+            contracts: ["console"],
+            includeFactories: true,
         },
-      },
-    },
-  },
-  typechain: {
-    outDir: "typechain",
-    target: "ethers-v5",
-  },
-};
+        paths: {
+            artifacts: "./artifacts",
+            cache: "./cache",
+            sources: "./contracts",
+            tests: "./test",
+        },
+        solidity: {
+            version: '0.8.0',
+            settings: {
+                metadata: {
+                    bytecodeHash: 'none',
+                },
+                optimizer: {
+                    enabled: false,
+                    runs: 200,
+                    details: {
+                        yul: false,
+                    },
+                    "outputSelection": {
+                        "*": {
+                            "*": [
+                                "metadata", "evm.bytecode", "evm.bytecode.functionDebugData"
+                            ],
+                            "": [
+                                "ast"
+                            ]
+                        },
+
+                    },
+
+                },
+            },
+            typechain: {
+                outDir: "typechain",
+                target: "ethers-v5",
+            },
+        }
+    }
 export default config;
+// @exports config
